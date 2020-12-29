db7275bac60eb208ea9cb0c813f91bc70c2af749
J-Broadway <jeremiah.broadway@hotmail.com>
2020-12-28 19:12:42 -0600

    tested append

--- test.py
+++ test.py
@@ -1,4 +1,28 @@
-import sys
 import csv
 
-print(sys.argv[1])
+#with open('test.csv', 'r', newline='') as csv_file:
+    # test = csv.writer(csv_file)
+    # test.writerow(['hey', 'there'])
+
+# file = open('test.csv', 'r')
+# lines = file.readlines()
+# for line in lines:
+#     if line == '\n':
+#         with open('test.csv', 'r', newline='') as csv_file:
+#             test = csv.writer(csv_file)
+#             test.writerow(['hey', 'there'])
+#     else:
+#         print('nope')
+
+# Add newline if there is no newline in file
+csv_file = open('test.csv', 'r')
+lines = csv_file.readlines()
+for line in lines:
+    if line.find('\n') < 0:
+        with open('test.csv', 'a', newline='') as csv_file:
+            add_enter = csv.writer(csv_file)
+            add_enter.writerow('')
+# If newline available append new directory to directories.csv
+with open('test.csv', 'a', newline='') as csv_file:
+    append = csv.writer(csv_file)
+    append.writerow(['hey', 'there'])
